c7b53a9953b054def8422bb08440e39707930771
atonixdev <devatonix@gmail.com>
2025-12-14 14:04:41 -0800

    commit 9f8b7c6d2e3a4b5c6d7e8f9a0b1c2d3e4f5g6h7i

--- backend/activity/apps.py
+++ backend/activity/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+import sys
 
 
 class ActivityConfig(AppConfig):
@@ -6,4 +7,7 @@ class ActivityConfig(AppConfig):
     name = 'activity'
 
     def ready(self):
+        # Avoid registering signals during migrations to prevent FK issues
+        if any(cmd in sys.argv for cmd in ('migrate', 'makemigrations', 'collectstatic')):
+            return
         from . import signals  # noqa
