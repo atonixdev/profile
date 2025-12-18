b47025c4c008eeb334a173079a1fa81ecb9a031a
atonixdev <devatonix@gmail.com>
2025-12-18 01:03:39 -0800

    commit Update settings.py with new configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-produc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ["atonixdev.org", "www.atonixdev.org", "api.atonixdev.org", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["atonixdev.org", "www.atonixdev.org", "api.atonixdev.org", "localhost", "127.0.0.1", "144.202.110.159"]
 
 # Application definition
 INSTALLED_APPS = [
