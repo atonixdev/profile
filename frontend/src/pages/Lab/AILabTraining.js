99854a6a021591c43a76a2cc7ba7aeb86527965d
atonixdev <ofidohub@gmail.com>
2026-03-09 21:33:26 -0700

    commit 9f8c7e5a2b3d4e5f6a7b8c9d0e1f2a3b4c5d6e

--- frontend/src/pages/Lab/AILabTraining.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import RunExperiment from './RunExperiment';
-
-const AILabTraining = () => {
-  return <RunExperiment experimentType="model_training" titleOverride="Model Training" />;
-};
-
-export default AILabTraining;
--- frontend/src/pages/Lab/AILabTuning.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Compare from './Compare';
-
-const AILabTuning = () => {
-  return <Compare experimentType="model_training" titleOverride="Hyperparameter Tuning" />;
-};
-
-export default AILabTuning;
